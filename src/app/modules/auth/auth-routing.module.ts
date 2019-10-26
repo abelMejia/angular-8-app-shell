a6b227ec7339f884bd539b457e8097860693c62e
abelMejia <abelm1500@gmail.com>
2019-10-26 18:47:39 -0500

    fix

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -4,7 +4,7 @@ import { LoginComponent } from './components/login/login.component';
 
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent, data: { title: 'Bienvenido a Osmos' } }
+  { path: 'login', component: LoginComponent, data: { title: 'Bienvenido' } }
 ];
 
 
