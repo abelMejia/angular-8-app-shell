68f7229ba99c87cf88dc2e9d02fe168cbd1c4af3
abel M <abel@osmos.mx>
2023-04-23 19:49:37 -0600

    client service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
+import { HttpParams } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Product } from './shared/interfaces/products/product.interface';
+import { ProductService } from './shared/services/products/products.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-app-s';
+    title = 'angular-app-s';
+    products$!: Observable<Product[]>;
+    productSearch$!: Observable<Product[]>;
+
+    constructor(private productService: ProductService) {}
+
+    ngOnInit(): void {
+        this.products$ = this.productService.getAll().pipe(map(it => it?.products.slice(0, 5) ));
+
+        let httpParams = new HttpParams();
+        httpParams = httpParams.append('q', 'laptop');
+        
+        this.productSearch$ = this.productService.search(httpParams).pipe(map(it => it?.products.slice(0, 5) ));
+    }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiEndpoint: '',
+    serverBaseUrl: 'https://dummyjson.com/',
+};
